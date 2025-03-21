857b41dd12e3b5d5836d4149fc592d45d8ceb649
Amankhan1009 <amankhan34356@gmail.com>
2025-03-22 00:16:44 +0530

    make it better

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask,render_template,request
 import pickle
 import numpy as np
 
-popular_df = pickle.load(open(r'C:\Book Recommender System\Model\popular.pkl','rb'))
-pt = pickle.load(open(r'C:\Book Recommender System\Model\pt.pkl','rb'))
-books = pickle.load(open(r'C:\Book Recommender System\Model\books.pkl','rb'))
-similarity_scores = pickle.load(open(r'C:\Book Recommender System\Model\similarity_scores.pkl','rb'))
+popular_df = pickle.load(open('popular.pkl','rb'))
+pt = pickle.load(open('pt.pkl','rb'))
+books = pickle.load(open('books.pkl','rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
 
 app = Flask(__name__)
 
